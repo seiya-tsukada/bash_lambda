18b043b15437567362f1097c9cc11f529e04937b
Tsukada <seiya.tsukada@accenture.com>
2018-12-07 11:28:08 +0900

    commit

--- sample/function.sh
+++ sample/function.sh
@@ -1,8 +1,11 @@
 function handler() {
+
+  set -x
+
   EVENT_DATA=$1
   echo "$EVENT_DATA" 1>&2
   VALUE=`echo $EVENT_DATA | $LAMBDA_TASK_ROOT/jq -r .key`
-  RESPONSE="get value: $VALUE !!"
+  RESPONSE="get value: ${VALUE}"
  
   echo $RESPONSE
 }
